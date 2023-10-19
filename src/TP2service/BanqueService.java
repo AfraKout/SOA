aa93f9e0962b5925ab6337a4b940188620a91de7
kouta <kouta@Afra>
2023-10-19 11:20:35 +0100

    commit

--- src/TP2service/BanqueService.java
+++ src/TP2service/BanqueService.java
@@ -1,6 +1,16 @@
 package TP2service;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BanqueService {
+		List<Compte> comptes = new ArrayList<Compte>();
+		public List<Compte> getComptes(){
+			return comptes;
+		}
+		
+		public Compte getCompte (int x) {
+			return comptes.get(x);
+		}
       public double conversion (double montant) {
     	  return montant *3.36;
       }
