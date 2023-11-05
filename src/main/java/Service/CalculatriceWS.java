68c09195d71fb3335ab25a20842fa783960e4494
AfraKout <koutafra8@gmail.com>
2023-11-05 19:53:41 +0100

    commit 4

---
+++ src/main/java/Service/CalculatriceWS.java
@@ -0,0 +1,13 @@
+package Service;
+
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebParam;
+import jakarta.jws.WebService;
+
+    @WebService
+    public class CalculatriceWS {
+        @WebMethod
+        public double somme(@WebParam double a,@WebParam double b){
+            return a+b;
+        }
+    }
